50a3fea2e77d1d5dda88b402d7988bd89a6dae70
te <tarek.elmeskine@eurekanetwork.org>
2018-07-23 20:15:06 +0200

    add children paths add page not found redirection when no resources found for a wrong url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,23 @@ import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
 import {Route, RouterModule} from '@angular/router';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
-const appRoutes: Route = [
+const appRoutes: Route [] = [
   {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'users/:id/:name', component: UserComponent},
-  {path: 'servers', component: ServersComponent},
-  {path: 'servers/:id/edit', component: EditServerComponent}
+  {
+    path: 'users', component: UsersComponent, children: [
+      {path: ':id/:name', component: UserComponent}
+    ]
+  },
+  {
+    path: 'servers', component: ServersComponent, children: [
+      {path: ':id', component: ServerComponent},
+      {path: ':id/edit', component: EditServerComponent}
+    ]
+  },
+  {path: 'not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/not-found'}
 ];
 
 @NgModule({
@@ -29,7 +39,8 @@ const appRoutes: Route = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
